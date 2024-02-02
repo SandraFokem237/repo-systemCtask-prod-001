f5d0f466e3dcc43c08e43c23eb64346515d7e6b6
SandraFokem <145476727+SandraFokem237@users.noreply.github.com>
2024-02-02 12:20:50 +0000

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,7 @@ namespace repo_systemCtask_prod_001
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddControllersWithViews();
         }
 
